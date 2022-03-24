254887852aac0ec2e5ab777d233f1af5a2ec95d6
lemkoland <98834856+lemkoland@users.noreply.github.com>
2022-03-24 11:26:21 +0100

    Update 404.vue

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -38,6 +38,15 @@
           }
         }
       }
+          allWordPressCategory (sortBy: "name", order: ASC) {
+  edges {
+    node {
+      title
+      id
+      path
+    }
+  }
+}
     }
   </page-query>
 
